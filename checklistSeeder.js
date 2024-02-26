d20e3a210afa6f47504b9040542f2610381473a5
Bryan Lilly <bryan.lilly@marlerintegrity.com>
2024-02-26 16:47:35 -0700

    seeder work

--- checklistSeeder.js
+++ checklistSeeder.js
@@ -18,8 +18,23 @@ const server = app.listen(PORT, console.log(`Server Running on PORT ${PORT}`));
 const seedChecklistItems = async(items) => {
     try {
         for(let item of items){
-            item.company = 'Marler';
-            item.tabName = 'Checklist'
+            item.company = 'TCB';
+            item.tabName = 'ShopField';
+            if(item.Period === 'Annually'){
+                item.Period = 'Annual'
+            }
+            if(item.Period === 'SemiAnnual' || item.Period === 'Semi Annual'){
+                item.Period = 'Semi-Annual'
+            }
+            if(item.Period === 'Semi-Monthly' || item.Period === 'Semi Monthly'){
+                item.Period = 'Bi-Monthly';
+            }
+            if(item.Period === 'Biweekly'){
+                item.Period = 'Bi-Weekly';
+            }
+            if(typeof(item['1st warn']) === 'string'){
+                item['1st warn'] = 1;
+            }
             await ChecklistItem.create(item);
             console.log('Item Created');
         }
@@ -28,5 +43,5 @@ const seedChecklistItems = async(items) => {
     }
 }
 
-const items = JSON.parse(fs.readFileSync(`${__dirname}/_data/item_data.json`, 'utf-8'));
+const items = JSON.parse(fs.readFileSync(`${__dirname}/_data/shopfield_tcb_data_renamed.json`, 'utf-8'));
 seedChecklistItems(items);
