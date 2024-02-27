98ce0ad9780d4dd01153045d88631ec896f52e0d
Bryan Lilly <bryan.lilly@marlerintegrity.com>
2024-02-27 16:59:59 -0700

    checklist controller and routes

--- controllers/Checklist/checklist.js
+++ controllers/Checklist/checklist.js
@@ -22,3 +22,46 @@ exports.getAllChecklistItems = asyncHandler(async(req, res, next) => {
         return next(new ErrorResponse(`Server Error - getAllChecklistItems`, 500))
     }
 });
+
+//@desc     Update a checklist item by ID
+//@route    GET /api/v1/checklist/:id
+//@access   Public
+exports.updateChecklistItem = asyncHandler(async(req, res, next) => {
+    try {
+        let item = await ChecklistItem.findByIdAndUpdate(req.params.id, req.body);
+
+        res.status(200).json({
+            success: true,
+            data: item
+        })
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorResponse(`Server Error - updateChecklistItem`, 500));
+    }
+});
+
+//@desc     Update an array of checklist items
+//@route    GET /api/v1/checklist/tabnames
+//@access   Public
+exports.updateMultipleItems = asyncHandler(async(req, res, next) => {
+    const itemsToUpdate = req.body;
+
+    const bulkOps = itemsToUpdate.map(item => ({
+        updateOne: {
+            filter: {_id: item._id},
+            update: {$set: item}
+        }
+    }));
+    
+    try {
+        const result = await ChecklistItem.bulkWrite(bulkOps);
+
+        res.status(200).json({
+            success: true,
+            data: result
+        });
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorResponse(`Server Error - updateMultipleItems`, 500));
+    }
+});
--- routes/Checklist/checklist.js
+++ routes/Checklist/checklist.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const {getAllChecklistItems} = require('../../controllers/Checklist/checklist');
+const {getAllChecklistItems, updateChecklistItem, updateMultipleItems} = require('../../controllers/Checklist/checklist');
 
 const router = express.Router();
 
 router
     .route('/')
     .get(getAllChecklistItems)
+
+router
+    .route('/tabnames')
+    .put(updateMultipleItems)
+
+router
+    .route('/:id')
+    .put(updateChecklistItem)
 
 module.exports = router;
