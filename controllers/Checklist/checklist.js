6443e5c5fe6bd498c646a4afe387f3179a3687d8
Bryan Lilly <bryan.lilly@marlerintegrity.com>
2024-02-28 15:14:55 -0700

    routes and controllers complete

--- controllers/Checklist/checklist.js
+++ controllers/Checklist/checklist.js
@@ -28,11 +28,17 @@ exports.getAllChecklistItems = asyncHandler(async(req, res, next) => {
 //@access   Public
 exports.updateChecklistItem = asyncHandler(async(req, res, next) => {
     try {
-        let item = await ChecklistItem.findByIdAndUpdate(req.params.id, req.body);
+        let item = await ChecklistItem.findById(req.params.id);
+
+        if(item['Last Done'] !== req.body['Last Done']){
+            await ItemCompleted.create({itemID: item._id, completedAt: req.body['Last Done']});
+        }
+
+        let newItem = await ChecklistItem.findByIdAndUpdate(req.params.id, req.body);
 
         res.status(200).json({
             success: true,
-            data: item
+            data: newItem
         })
     } catch (error) {
         console.log(error);
@@ -63,5 +69,68 @@ exports.updateMultipleItems = asyncHandler(async(req, res, next) => {
     } catch (error) {
         console.log(error);
         return next(new ErrorResponse(`Server Error - updateMultipleItems`, 500));
+    }
+});
+
+//@desc     Create a new checklist item
+//@route    POST /api/v1/checklist
+//@access   Public
+exports.createNewChecklistItem = asyncHandler(async(req, res, next) => {
+    try {
+        let item = await ChecklistItem.create(req.body);
+
+        if(!item) return next(new ErrorResponse(`Error Creating Checklist Item`, 400));
+
+        res.status(200).json({
+            success: true,
+            data: item
+        })
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorResponse(`Server Error - createNewChecklistItem`, 500));
+    }
+});
+
+//@desc     Delete a checklist item
+//@route    DELETE /api/v1/checklist/:id
+//@access   Public
+exports.deleteChecklistItem = asyncHandler(async(req, res, next) => {
+    try {
+        let item = await ChecklistItem.findByIdAndDelete(req.params.id);
+
+        if(!item) return next(new ErrorResponse(`Error deleting Checklist Item with ID ${req.params.id}`, 400));
+
+        res.status(200).json({
+            success: true,
+            data: item
+        })
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorResponse(`Server Error - createNewChecklistItem`, 500));
+    }
+});
+
+//@desc     Delete an array of checklist items - tab has been removed
+//@route    DELETE /api/v1/checklist/tabremoved
+//@access   Public
+exports.deleteArrayOfItems = asyncHandler(async(req, res, next) => {
+    const itemsToDelete = req.body;
+
+    const bulkOps = itemsToDelete.map((item) => ({
+        deleteOne: {
+            filter: {_id: item._id}
+        }
+    }));
+    
+    try {
+        const result = await ChecklistItem.bulkWrite(bulkOps);
+
+        res.status(200).json({
+            success: true,
+            data: result
+        })
+    } catch (error) {
+        console.log(error);
+        return next(new ErrorResponse('Server Error - deleteArrayOfItems'), 500);
     }
 });
--- routes/Checklist/checklist.js
+++ routes/Checklist/checklist.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const {getAllChecklistItems, updateChecklistItem, updateMultipleItems} = require('../../controllers/Checklist/checklist');
+const {
+    getAllChecklistItems, 
+    updateChecklistItem, 
+    updateMultipleItems, 
+    createNewChecklistItem, 
+    deleteChecklistItem, 
+    deleteArrayOfItems
+} = require('../../controllers/Checklist/checklist');
 
 const router = express.Router();
 
 router
     .route('/')
     .get(getAllChecklistItems)
+    .post(createNewChecklistItem)
 
 router
     .route('/tabnames')
     .put(updateMultipleItems)
 
 router
+    .route('/tabremoved')
+    .delete(deleteArrayOfItems)
+
+router
     .route('/:id')
     .put(updateChecklistItem)
+    .delete(deleteChecklistItem)
 
 module.exports = router;
