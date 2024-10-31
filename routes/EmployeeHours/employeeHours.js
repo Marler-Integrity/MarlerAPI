20f6eb978e75422ddbbd0393450bd3c009841c81
Julia Hack <julia.hack@marlerintegrity.com>
2024-10-31 15:23:21 -0600

    Add register endpoint for employeeHours; rearrange params in ErrorResponse that were copied from martrak2

--- routes/EmployeeHours/employeeHours.js
+++ routes/EmployeeHours/employeeHours.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { userLogin } = require('../../controllers/EmployeeHours/auth/auth');
+const { userLogin, userRegister } = require('../../controllers/EmployeeHours/auth/auth');
 
 const router = express.Router();
 
 router
     .route('/auth/login')
     .post(userLogin)
+    
+router
+    .route('/auth/register')
+    .post(userRegister)
 
 module.exports = router;
