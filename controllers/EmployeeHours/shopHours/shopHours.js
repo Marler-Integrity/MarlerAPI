003973549b6665c900ba8ecca22bc52235b1fe79
Julia Hack <julia.hack@marlerintegrity.com>
2024-11-05 17:44:04 -0700

    New categories route; new controller to get raw entries

--- controllers/EmployeeHours/shopHours/shopHours.js
+++ controllers/EmployeeHours/shopHours/shopHours.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("../../../middleware/async");
 const createMasterRawEntryModel = require("../../../models/EmployeeHours/MasterRawEntry");
 const createSubmittedRawDataModel = require("../../../models/EmployeeHours/SubmittedRawData");
+const createPeopleModel = require("../../../models/EmployeeHours/People");
 const ErrorResponse = require("../../../utils/ErrorResponse");
 
 
@@ -54,5 +55,60 @@ exports.submitShopHours = asyncHandler(async (req, res, next) => {
     } catch (error) {
         await t.rollback();
         return next(new ErrorResponse(`Server Error - submitShopHours - ${error.message}`, 500));
+    }
+});
+
+/**
+ * @description gets all active entries
+ */
+exports.getAllEntries = asyncHandler(async (req, res, next) => {
+
+    //Step 1: set up models
+    const MasterRawEntry = createMasterRawEntryModel(req.db);
+    const SubmittedRawData = createSubmittedRawDataModel(req.db);
+    const People = createPeopleModel(req.db);
+
+    // Step 2: get all master entries - assuming that old master entries will be archived and these are all current
+    const masterRawEntries = await MasterRawEntry.findAll({raw:true});
+
+    // Step 3: get peopleIds and masterIds from masterRawEntries
+    const peopleIds = [...new Set(masterRawEntries.map(entry => entry.PeopleID))]; 
+    const masterIds = masterRawEntries.map(entry => entry.MasterID);
+    
+    // Step 4: get people and submittedRawData
+    const [people, submittedData] = await Promise.all([
+        People.findAll({
+            where: { PersonID: peopleIds },
+            attributes: ['FirstName', 'LastName', 'PersonID'],
+            raw: true,
+        }),
+        SubmittedRawData.findAll({
+            where: { MasterID: masterIds },
+            raw: true,
+        }),
+    ]);
+
+    // Step 5: create result object of submitted raw entries, with person data and entry date from master entry
+    const result = submittedData.map(entry => {
+
+        const masterEntry = masterRawEntries.find(masterEntry => masterEntry.MasterID === entry.MasterID)
+        const person = people.find(person => person.PersonID === masterEntry.PeopleID)
+ 
+        return {
+            ...entry,
+            ...person,
+            EntryDate: masterEntry.EntryDate
+        };
+    });
+
+    try {
+        
+        res.status(200).json({
+            success: true,
+            data: result
+        });
+    } catch (error) {
+        console.log(error)
+        return next(new ErrorResponse(`Server Error - getAllEntries - ${error.message}`, 500));
     }
 });
--- routes/EmployeeHours/employeeHours.js
+++ routes/EmployeeHours/employeeHours.js
@@ -3,9 +3,10 @@ const express = require('express');
 
 const { getAllPeople } = require('../../controllers/EmployeeHours/resources/people');
 const { getAllJobs } = require('../../controllers/EmployeeHours/resources/jobs');
+const { getAllCategories } = require('../../controllers/EmployeeHours/resources/categories')
 const upload = require('../../middleware/fileUpload');
 const { importInternalJobList } = require('../../controllers/EmployeeHours/imports/importData');
-const { submitShopHours } = require('../../controllers/EmployeeHours/shopHours/shopHours');
+const { submitShopHours, getAllEntries } = require('../../controllers/EmployeeHours/shopHours/shopHours');
 
 const { userLogin, userRegister } = require('../../controllers/EmployeeHours/auth/auth');
 
@@ -27,6 +28,14 @@ router
 router
     .route('/jobs')
     .get(getAllJobs)
+
+router
+    .route('/categories')
+    .get(getAllCategories)
+
+router
+    .route('/shop/entries')
+    .get(getAllEntries)
 
 router
     .route('/shop/submit')
