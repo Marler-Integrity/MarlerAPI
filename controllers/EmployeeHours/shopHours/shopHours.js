1b244ee49ddac28f43a6b4fb51797628b9bca6f5
Bryan Lilly <bryan.lilly@marlerintegrity.com>
2024-11-12 11:01:41 -0700

    Fixed controller to match new db schema

--- controllers/EmployeeHours/shopHours/shopHours.js
+++ controllers/EmployeeHours/shopHours/shopHours.js
@@ -1,8 +1,8 @@
 const asyncHandler = require("../../../middleware/async");
-const createMasterRawEntryModel = require("../../../models/EmployeeHours/MasterRawEntry");
+// const createMasterRawEntryModel = require("../../../models/EmployeeHours/MasterRawEntry");
 const createSubmittedRawDataModel = require("../../../models/EmployeeHours/SubmittedRawData");
 const ErrorResponse = require("../../../utils/ErrorResponse");
-
+const { v4: uuidv4 } = require('uuid');
 
 /**
  * @route  POST /api/v1/employeehours/shop/submit
@@ -12,49 +12,36 @@ exports.submitShopHours = asyncHandler(async (req, res, next) => {
     let t = await req.db.transaction()
     try {
         const SubmittedRawData = createSubmittedRawDataModel(req.db);
-        const MasterRawEntry = createMasterRawEntryModel(req.db);
-
+        
         const { EntryDate, PeopleID, entries } = req.body;  //entries is an array of objects - each object is an entry 
 
         if (!entries.length) return next(new ErrorResponse(`There are no Entries in Request`, 400));
         if (!EntryDate || !PeopleID) return next(new ErrorResponse(`Request Must Include Date and Person`));
 
-        let masterEntry = null;
-        try {
-            console.log(EntryDate)
-            const formatSQLDate = (date) => {
-                // console.log(date)
-                // const localDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000);
-                return date.toISOString();
-            };
+        const formatSQLDate = (date) => {
+            return date.toISOString();
+        };
 
-            masterEntry = await MasterRawEntry.create({ EntryDate: formatSQLDate(new Date(EntryDate)), PeopleID }, { transaction: t });
-        } catch (error) {
-            console.log(error)
-            throw new Error(`Error Creating Master Raw Entry - ${error.message}`);
+        const masterId = uuidv4();
+
+        for (let entry of entries) {
+            await SubmittedRawData.create(
+                {
+                    ...entry,  // Spread entry data
+                    EntryDate: formatSQLDate(new Date(EntryDate)),  // Format date
+                    PeopleID: PeopleID,
+                    MasterID: masterId  // Generate unique ID
+                },
+                { transaction: t }  // Ensure each create operation is part of the transaction
+            );
         }
-
-        const masterEntryID = masterEntry.MasterID;
-
-        // Create all entries in the SubmittedRawData table using `Promise.all`
-        await Promise.all(
-            entries.map(async (entry) => {
-                // Add `masterEntryID` to each entry as a foreign key, if needed
-                await SubmittedRawData.create(
-                    {
-                        ...entry, // Spread the entry data
-                        MasterID: masterEntryID // Add the reference to the master entry
-                    },
-                    { transaction: t }
-                );
-            })
-        );
 
         await t.commit();
         res.status(200).json({
             success: true
         });
     } catch (error) {
+        console.log(error)
         await t.rollback();
         return next(new ErrorResponse(`Server Error - submitShopHours - ${error.message}`, 500));
     }
@@ -67,23 +54,17 @@ exports.submitShopHours = asyncHandler(async (req, res, next) => {
 exports.getEmployeeHourSubmissions = asyncHandler(async (req, res, next) => {
     const peopleID = req.params.peopleid;
     try {
-        const MasterRawEntry = createMasterRawEntryModel(req.db);
+        // const MasterRawEntry = createMasterRawEntryModel(req.db);
         const SubmittedRawData = createSubmittedRawDataModel(req.db);
 
         // Define associations
-        if (!MasterRawEntry.associations.SubmittedRawData) {
-            MasterRawEntry.hasMany(SubmittedRawData, { foreignKey: 'MasterID' });
-            SubmittedRawData.belongsTo(MasterRawEntry, { foreignKey: 'MasterID' });
-        }
+        // if (!MasterRawEntry.associations.SubmittedRawData) {
+        //     MasterRawEntry.hasMany(SubmittedRawData, { foreignKey: 'MasterID' });
+        //     SubmittedRawData.belongsTo(MasterRawEntry, { foreignKey: 'MasterID' });
+        // }
 
-        let employeeHourSubmissions = await MasterRawEntry.findAll({
-            where: { PeopleID: peopleID },
-            include: [
-                {
-                    model: SubmittedRawData,
-                    required: false
-                }
-            ]
+        let employeeHourSubmissions = await SubmittedRawData.findAll({
+            where: { PeopleID: peopleID }
         });
 
         res.status(200).json({
