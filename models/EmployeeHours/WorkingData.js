fa647f066a6ead0a0ed6468c37ca085ae9f14f53
Julia Hack <julia.hack@marlerintegrity.com>
2024-11-19 17:47:25 -0700

    Updated SubmittedRawData and WorkingData to use floats for number of hours; added IsCopy to working data

--- models/EmployeeHours/WorkingData.js
+++ models/EmployeeHours/WorkingData.js
@@ -1,58 +1,64 @@
-const {DataTypes} = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 const createWorkingDataModel = (dbInstance) => {
-    const WorkingData = dbInstance.define('WorkingData', {
-        WorkingDataID: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-       LastName: {
-            type: DataTypes.STRING,
-       },
-       FirstName: {
-            type: DataTypes.STRING,
-       },
-       JobName: {
-            type: DataTypes.STRING,
-       },
-       NumHours: {
-            type: DataTypes.TINYINT
-       },
-       Description: {
-            type: DataTypes.STRING,
-       },
-       SAPCategory: {
-            type: DataTypes.INTEGER,
-       },
-       Notes: {
-            type: DataTypes.STRING,
-       },
-       Regular: {
-          type: DataTypes.INTEGER,
-       },
-       OT: {
-          type: DataTypes.INTEGER,
-       },
-       SRDID: {
-            type: DataTypes.INTEGER,
-       },
-       PeopleID: {
-            type: DataTypes.INTEGER,
-       },
-       EntryDate: {
-            type: DataTypes.DATE,
-       },
-       FrontEndID: {
-        type: DataTypes.STRING, 
-       }
+  const WorkingData = dbInstance.define(
+    "WorkingData",
+    {
+      WorkingDataID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      LastName: {
+        type: DataTypes.STRING,
+      },
+      FirstName: {
+        type: DataTypes.STRING,
+      },
+      JobName: {
+        type: DataTypes.STRING,
+      },
+      NumHours: {
+        type: DataTypes.FLOAT,
+      },
+      Description: {
+        type: DataTypes.STRING,
+      },
+      SAPCategory: {
+        type: DataTypes.INTEGER,
+      },
+      Notes: {
+        type: DataTypes.STRING,
+      },
+      Regular: {
+        type: DataTypes.FLOAT,
+      },
+      OT: {
+        type: DataTypes.FLOAT,
+      },
+      SRDID: {
+        type: DataTypes.INTEGER,
+      },
+      PeopleID: {
+        type: DataTypes.INTEGER,
+      },
+      EntryDate: {
+        type: DataTypes.DATE,
+      },
+      FrontEndID: {
+        type: DataTypes.STRING,
+      },
+      IsCopy: {
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
 
-    }, {
-        timestamps: false
-    });
-
-    return WorkingData;
-}
+  return WorkingData;
+};
 
 module.exports = createWorkingDataModel;
