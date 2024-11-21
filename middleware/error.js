379ae96a8b1872249f8fcb5aeec4afe6bcbfd80f
Bryan Lilly <bryan.lilly@marlerintegrity.com>
2024-11-21 16:32:58 -0700

    error debugging

--- middleware/error.js
+++ middleware/error.js
@@ -4,6 +4,7 @@ const ErrorResponse = require('../utils/ErrorResponse');
 
 //custom error handler
 const errorHandler = (err, req, res, next) => {
+    console.log(err)
     let error = { ...err };
 
     error.message = err.message;
